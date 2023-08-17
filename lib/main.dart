95f3338551bb26751f8186a399073910c73f0e89
Zeereoreo <wltjrcjf12@naver.com>
2023-08-17 11:07:23 +0900

    flexible Expanded 사용법

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,30 +13,16 @@ class MyApp extends StatelessWidget {
     return
       MaterialApp(
           home: Scaffold(
-            appBar: AppBar( actions: [Icon(Icons.star),Icon(Icons.star)], title: Text('앱제목'), leading: Icon(Icons.star)),
-            // title : 왼쪽 제목, leading : 왼쪽에 넣을 아이콘 , actions [우측아이콘들]
-            body: SizedBox(
-              child: Text('안녕하세요', //파라미터 끝날 떄 콤마 잊지말기 
-                // style: TextStyle( color : Color.fromRGBO(r, g, b, opacity) 컬러
-                // style: TextStyle(fontWeight: FontWeight.w700), 글자 크기
-                // child: Icon(Icons.star, size:20,)아이콘은 사이즈가 끝
-                // child: ElevatedButton( child: Text('버튼'),
-                //   onPressed:  (){},
-                //   style: ,
-                // ) 버튼 넣는 방법 TextButton(),IconButton(),ElevatedButton()
-              ),
+            appBar: AppBar(),
+            body: Row(
+              children: [
+                Expanded(child: Container(color: Colors.blue)),
+                Flexible(flex: 7, child: Container(color: Colors.green)),
+                //비율에 따라 박스를 넣고 싶을 떄 사용 Column,Row에서 사용
+                //Expanded는 나머지 꽉 채우고 싶을 때 사용
+              ],
             ),
-            ),
-
-            // body: Text('안녕'),
-            // bottomNavigationBar: SizedBox(
-            //   height: 50,
-            //   child: Row(
-            //       mainAxisAlignment: MainAxisAlignment.spaceAround,
-            //       crossAxisAlignment: CrossAxisAlignment.start,
-            //       children: [ Icon(Icons.phone), Icon(Icons.message), Icon(Icons.contact_page) ]
-            //   ),
-            
+          )
       );
   }
 }
