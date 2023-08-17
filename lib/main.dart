4e79303943997f3c7e76a7aba464cd6476310a23
Zeereoreo <wltjrcjf12@naver.com>
2023-08-17 10:29:49 +0900

    Container 사용법

--- lib/main.dart
+++ lib/main.dart
@@ -13,13 +13,28 @@ class MyApp extends StatelessWidget {
       MaterialApp(
           home: Scaffold(
             appBar: AppBar( title: Text('앱제목') ),
-            body: Text('안녕'),
-            bottomNavigationBar: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [ Icon(Icons.phone), Icon(Icons.message), Icon(Icons.contact_page) ]
+            body: Align(
+              alignment: Alignment.center,
+              child: Container(
+                width: double.infinity,height: 50,
+                margin: EdgeInsets.all(20),
+                // margin: EdgeInsets.fromLTRB(left, top, right, bottom)
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black), //컬러중복은 안됨
+                ),
+
+              ),
             ),
-          )
+
+            // body: Text('안녕'),
+            // bottomNavigationBar: SizedBox(
+            //   height: 50,
+            //   child: Row(
+            //       mainAxisAlignment: MainAxisAlignment.spaceAround,
+            //       crossAxisAlignment: CrossAxisAlignment.start,
+            //       children: [ Icon(Icons.phone), Icon(Icons.message), Icon(Icons.contact_page) ]
+            //   ),
+            ),
       );
   }
 }
