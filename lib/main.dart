69d2350373d02f0ee72fefb8c04baf41f72d174f
Zeereoreo <wltjrcjf12@naver.com>
2023-08-17 10:42:43 +0900

    AppBar 세부내용

--- lib/main.dart
+++ lib/main.dart
@@ -12,18 +12,19 @@ class MyApp extends StatelessWidget {
     return
       MaterialApp(
           home: Scaffold(
-            appBar: AppBar( title: Text('앱제목') ),
-            body: Align(
-              alignment: Alignment.center,
-              child: Container(
-                width: double.infinity,height: 50,
-                margin: EdgeInsets.all(20),
-                // margin: EdgeInsets.fromLTRB(left, top, right, bottom)
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black), //컬러중복은 안됨
-                ),
-
+            appBar: AppBar( actions: [Icon(Icons.star),Icon(Icons.star)], title: Text('앱제목'), leading: Icon(Icons.star)),
+            // title : 왼쪽 제목, leading : 왼쪽에 넣을 아이콘 , actions [우측아이콘들]
+            body: SizedBox(
+              child: Text('안녕하세요', //파라미터 끝날 떄 콤마 잊지말기 
+                // style: TextStyle( color : Color.fromRGBO(r, g, b, opacity) 컬러
+                // style: TextStyle(fontWeight: FontWeight.w700), 글자 크기
+                // child: Icon(Icons.star, size:20,)아이콘은 사이즈가 끝
+                // child: ElevatedButton( child: Text('버튼'),
+                //   onPressed:  (){},
+                //   style: ,
+                // ) 버튼 넣는 방법 TextButton(),IconButton(),ElevatedButton()
               ),
+            ),
             ),
 
             // body: Text('안녕'),
@@ -34,7 +35,7 @@ class MyApp extends StatelessWidget {
             //       crossAxisAlignment: CrossAxisAlignment.start,
             //       children: [ Icon(Icons.phone), Icon(Icons.message), Icon(Icons.contact_page) ]
             //   ),
-            ),
+            
       );
   }
 }
